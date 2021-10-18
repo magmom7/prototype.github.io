8c121ff8a6e21c4ee3631484cfd0e3bd16dc6433
magmom7 <84061081+magmom7@users.noreply.github.com>
2021-10-18 23:48:01 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,117 @@
+const qnaList = [
+  {
+    q: '1. 이성규를 존경하나요',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '2. Js는 비동기인가요',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '3. 이성규 밥을 사주실건가요?',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '4. 이성규에게 돈을 줄건가요?',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '5. JS는 단일쓰레드인가요?',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '6. o를 눌러주세요',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '7. 이런식으로 합산을 해서',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '8. 결과값 창출',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '9. 한다는 소리죠 10문제로',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  },
+  {
+    q: '10. 모르면 연락을 ',
+    a: [
+      { answer: 'a. o', score: 10 },
+      { answer: 'b. x', score: 0 },
+    ]
+  }
+]
+
+const infoList = [
+  {
+    from: 0,
+    to: 20,
+    mLeft: '10%',
+    name: '재시험',
+    desc: '성공까지 다시 재시험 보세요'
+  },
+  {
+    from: 21,
+    to: 40,
+    mLeft: '25%',
+    name: '재시험',
+    desc: '성공까지 다시 재시험 보세요'
+  },
+  {
+    from: 41,
+    to: 60,
+    mLeft: '40%',
+    name: '재시험',
+    desc: '성공까지 다시 재시험 보세요'
+  },
+  {
+    from: 61,
+    to: 70,
+    mLeft: '60%',
+    name: '재시험',
+    desc: '성공까지 다시 재시험 보세요'
+  },
+  {
+    from: 71,
+    to: 80,
+    mLeft: '80%',
+    name: '합격',
+    desc: '인증샷을 찍어 보내주세요'
+  },
+  {
+    from: 81,
+    to: 100,
+    mLeft: '100%',
+    name: '합격',
+    desc: '인증샷을 찍어 보내주세요 시크릿 단어는 왓슨입니다.'
+  }
+]
